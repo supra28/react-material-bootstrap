de9d363c81d786e0f62162eac03e45ad5f0ad657
Harshit Mehrotra <harshitmehrotra@hotmail.com>
2017-02-05 22:31:20 +0530

    first

---
+++ src/components/About/About.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import './About.css';
+
+export default class About extends Component {
+  render() {
+    return (
+      <div className="container">
+       <h1>
+          About
+        </h1>
+      </div>
+    );
+  }
+}
