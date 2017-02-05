eb9b7c3afed842e34e05615b2b38bc450ada4454
Harshit Mehrotra <harshitmehrotra@hotmail.com>
2017-02-06 00:09:14 +0530

    added material-ui

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,19 +1,32 @@
 import React, { Component } from 'react';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import RaisedButton from 'material-ui/RaisedButton';
 import logo from './logo.svg';
 import styles from './App.css'
 
+injectTapEventPlugin();
+
+
+
+
 class App extends Component {
+  state = { greetData: "Hello" }
   render() {
+    var {greetData} = this.state;
     return (
+      <MuiThemeProvider>
       <div className="app">
         <div className={styles.AppHeader}>
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          {greetData}, To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <RaisedButton label="Default"/>
       </div>
+    </MuiThemeProvider>
     );
   }
 }
